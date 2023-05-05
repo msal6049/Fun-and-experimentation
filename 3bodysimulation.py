665e429323a78b6f2d5344b3019c63c848c49551
Hiro <114344968+msal6049@users.noreply.github.com>
2023-05-05 10:19:04 +0100

    Update 3bodysimulation.py

--- 3bodysimulation.py
+++ 3bodysimulation.py
@@ -1,1 +1,102 @@
 # a 3 body simulation
+
+"""
+Sure! Here's an example of a 3-body simulation in Python using the `vpython` library for visualization and the `tkinter` library for the GUI. This program lets you input the mass and initial position and velocity of three bodies, and simulates their motion under the influence of gravity. 
+
+```python
+from vpython import *
+import tkinter as tk
+
+# create a tkinter window for user input
+root = tk.Tk()
+root.title("3-Body Simulation")
+
+# create variables for user input
+masses = []
+positions = []
+velocities = []
+
+# function to add a new body to the simulation
+def add_body():
+    mass = float(mass_entry.get())
+    x = float(x_entry.get())
+    y = float(y_entry.get())
+    z = float(z_entry.get())
+    vx = float(vx_entry.get())
+    vy = float(vy_entry.get())
+    vz = float(vz_entry.get())
+    masses.append(mass)
+    positions.append(vector(x, y, z))
+    velocities.append(vector(vx, vy, vz))
+    # clear the input fields for the next body
+    mass_entry.delete(0, END)
+    x_entry.delete(0, END)
+    y_entry.delete(0, END)
+    z_entry.delete(0, END)
+    vx_entry.delete(0, END)
+    vy_entry.delete(0, END)
+    vz_entry.delete(0, END)
+    
+# create labels and entry fields for user input
+mass_label = tk.Label(root, text="Mass:")
+mass_label.grid(row=0, column=0)
+mass_entry = tk.Entry(root)
+mass_entry.grid(row=0, column=1)
+
+x_label = tk.Label(root, text="x:")
+x_label.grid(row=1, column=0)
+x_entry = tk.Entry(root)
+x_entry.grid(row=1, column=1)
+
+y_label = tk.Label(root, text="y:")
+y_label.grid(row=2, column=0)
+y_entry = tk.Entry(root)
+y_entry.grid(row=2, column=1)
+
+z_label = tk.Label(root, text="z:")
+z_label.grid(row=3, column=0)
+z_entry = tk.Entry(root)
+z_entry.grid(row=3, column=1)
+
+vx_label = tk.Label(root, text="vx:")
+vx_label.grid(row=4, column=0)
+vx_entry = tk.Entry(root)
+vx_entry.grid(row=4, column=1)
+
+vy_label = tk.Label(root, text="vy:")
+vy_label.grid(row=5, column=0)
+vy_entry = tk.Entry(root)
+vy_entry.grid(row=5, column=1)
+
+vz_label = tk.Label(root, text="vz:")
+vz_label.grid(row=6, column=0)
+vz_entry = tk.Entry(root)
+vz_entry.grid(row=6, column=1)
+
+add_button = tk.Button(root, text="Add Body", command=add_body)
+add_button.grid(row=7, column=0, columnspan=2)
+
+# set up the 3D scene
+scene = canvas(width=800, height=600)
+scene.camera.pos = vector(0, 0, 0)
+scene.camera.axis = vector(0, 0, -1)
+
+# create the bodies
+bodies = []
+for i in range(3):
+    body = sphere(pos=positions[i], radius=0.5, make_trail=True)
+    body.mass = masses[i]
+    body.velocity = velocities[i]
+    bodies.append(body)
+
+# define the gravitational constant
+G = 6.674e-11
+
+# define the time step and simulation duration
+dt = 0.01
+t = 0
+duration = 1000
+
+#
+
+"""
